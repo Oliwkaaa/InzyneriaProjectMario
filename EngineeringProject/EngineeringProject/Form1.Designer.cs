3372c45d66a5b6484e41578d9a88e5514900957f
ElGrell <48662508+ElGrell@users.noreply.github.com>
2019-03-26 10:40:35 +0200

    Add files via upload
    Version 0.01 (not operatable yet).

---
+++ EngineeringProject/EngineeringProject/Form1.Designer.cs
@@ -0,0 +1,290 @@
+﻿namespace EngineeringProject
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.WorldFrame = new System.Windows.Forms.PictureBox();
+            this.WorldFloor = new System.Windows.Forms.PictureBox();
+            this.pb_Block1 = new System.Windows.Forms.PictureBox();
+            this.pb_Block2 = new System.Windows.Forms.PictureBox();
+            this.pb_Player = new System.Windows.Forms.PictureBox();
+            this.label_Score = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.WorldFrame)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.WorldFloor)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb_Block1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb_Block2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb_Player)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // WorldFrame
+            // 
+            this.WorldFrame.BackColor = System.Drawing.Color.SkyBlue;
+            this.WorldFrame.Dock = System.Windows.Forms.DockStyle.Top;
+            this.WorldFrame.Location = new System.Drawing.Point(0, 0);
+            this.WorldFrame.Name = "WorldFrame";
+            this.WorldFrame.Size = new System.Drawing.Size(584, 250);
+            this.WorldFrame.TabIndex = 0;
+            this.WorldFrame.TabStop = false;
+            // 
+            // WorldFloor
+            // 
+            this.WorldFloor.BackColor = System.Drawing.Color.SaddleBrown;
+            this.WorldFloor.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.WorldFloor.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.WorldFloor.Location = new System.Drawing.Point(0, 249);
+            this.WorldFloor.Name = "WorldFloor";
+            this.WorldFloor.Size = new System.Drawing.Size(584, 50);
+            this.WorldFloor.TabIndex = 1;
+            this.WorldFloor.TabStop = false;
+            // 
+            // pb_Block1
+            // 
+            this.pb_Block1.BackColor = System.Drawing.Color.ForestGreen;
+            this.pb_Block1.Location = new System.Drawing.Point(68, 187);
+            this.pb_Block1.Name = "pb_Block1";
+            this.pb_Block1.Size = new System.Drawing.Size(219, 26);
+            this.pb_Block1.TabIndex = 2;
+            this.pb_Block1.TabStop = false;
+            // 
+            // pb_Block2
+            // 
+            this.pb_Block2.BackColor = System.Drawing.Color.ForestGreen;
+            this.pb_Block2.Location = new System.Drawing.Point(319, 149);
+            this.pb_Block2.Name = "pb_Block2";
+            this.pb_Block2.Size = new System.Drawing.Size(237, 26);
+            this.pb_Block2.TabIndex = 3;
+            this.pb_Block2.TabStop = false;
+            // 
+            // pb_Player
+            // 
+            this.pb_Player.BackColor = System.Drawing.Color.Transparent;
+            this.pb_Player.Image = global::EngineeringProject.Properties.Resources.stand_r;
+            this.pb_Player.Location = new System.Drawing.Point(12, 127);
+            this.pb_Player.Name = "pb_Player";
+            this.pb_Player.Size = new System.Drawing.Size(16, 32);
+            this.pb_Player.TabIndex = 4;
+            this.pb_Player.TabStop = false;
+            // 
+            // label_Score
+            // 
+            this.label_Score.AutoSize = true;
+            this.label_Score.BackColor = System.Drawing.Color.Transparent;
+            this.label_Score.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label_Score.Location = new System.Drawing.Point(460, 9);
+            this.label_Score.Name = "label_Score";
+            this.label_Score.Size = new System.Drawing.Size(80, 24);
+            this.label_Score.TabIndex = 5;
+            this.label_Score.Text = "Score: 0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(584, 299);
+            this.Controls.Add(this.label_Score);
+            this.Controls.Add(this.pb_Player);
+            this.Controls.Add(this.pb_Block2);
+            this.Controls.Add(this.pb_Block1);
+            this.Controls.Add(this.WorldFloor);
+            this.Controls.Add(this.WorldFrame);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.WorldFrame)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.WorldFloor)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb_Block1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb_Block2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb_Player)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.WorldFrame = new System.Windows.Forms.Panel();
+            this.pb_Block1 = new System.Windows.Forms.PictureBox();
+            this.pb_Block2 = new System.Windows.Forms.PictureBox();
+            this.pb_Pipe = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label_Score = new System.Windows.Forms.Label();
+            this.pb_Player = new System.Windows.Forms.PictureBox();
+            this.timer_Gravity = new System.Windows.Forms.Timer(this.components);
+            this.timer_Jump = new System.Windows.Forms.Timer(this.components);
+            this.timer_Anim = new System.Windows.Forms.Timer(this.components);
+            this.timer_Sec = new System.Windows.Forms.Timer(this.components);
+            this.timerBoomRemove = new System.Windows.Forms.Timer(this.components);
+            this.WorldFloor = new System.Windows.Forms.Panel();
+            this.WorldFrame.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pb_Block1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb_Block2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb_Player)).BeginInit();
+            this.WorldFloor.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // WorldFrame
+            // 
+            this.WorldFrame.BackColor = System.Drawing.Color.SkyBlue;
+            this.WorldFrame.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.WorldFrame.Controls.Add(this.pb_Block1);
+            this.WorldFrame.Controls.Add(this.pb_Block2);
+            this.WorldFrame.Controls.Add(this.label2);
+            this.WorldFrame.Controls.Add(this.label1);
+            this.WorldFrame.Controls.Add(this.label_Score);
+            this.WorldFrame.Controls.Add(this.pb_Player);
+            this.WorldFrame.Dock = System.Windows.Forms.DockStyle.Top;
+            this.WorldFrame.Location = new System.Drawing.Point(0, 0);
+            this.WorldFrame.Name = "WorldFrame";
+            this.WorldFrame.Size = new System.Drawing.Size(656, 241);
+            this.WorldFrame.TabIndex = 0;
+            // 
+            // pb_Block1
+            // 
+            this.pb_Block1.BackColor = System.Drawing.Color.Gray;
+            this.pb_Block1.BackgroundImage = global::BombsAway.World.Platform;
+            this.pb_Block1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pb_Block1.Location = new System.Drawing.Point(451, 182);
+            this.pb_Block1.Name = "pb_Block1";
+            this.pb_Block1.Size = new System.Drawing.Size(151, 24);
+            this.pb_Block1.TabIndex = 8;
+            this.pb_Block1.TabStop = false;
+            // 
+            // pb_Block2
+            // 
+            this.pb_Block2.BackColor = System.Drawing.Color.Gray;
+            this.pb_Block2.BackgroundImage = global::BombsAway.World.Platform;
+            this.pb_Block2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pb_Block2.Location = new System.Drawing.Point(38, 157);
+            this.pb_Block2.Name = "pb_Block2";
+            this.pb_Block2.Size = new System.Drawing.Size(135, 24);
+            this.pb_Block2.TabIndex = 7;
+            this.pb_Block2.TabStop = false;
+            // 
+            // label_Score
+            // 
+            this.label_Score.AutoSize = true;
+            this.label_Score.BackColor = System.Drawing.Color.Transparent;
+            this.label_Score.Font = new System.Drawing.Font("Consolas", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label_Score.Location = new System.Drawing.Point(4, 9);
+            this.label_Score.Name = "label_Score";
+            this.label_Score.Size = new System.Drawing.Size(55, 13);
+            this.label_Score.TabIndex = 2;
+            this.label_Score.Text = "Score: 0";
+            // 
+            // pb_Player
+            // 
+            this.pb_Player.BackColor = System.Drawing.Color.Transparent;
+            this.pb_Player.Image = global::BombsAway.Character.stand_r;
+            this.pb_Player.Location = new System.Drawing.Point(12, 94);
+            this.pb_Player.Name = "pb_Player";
+            this.pb_Player.Size = new System.Drawing.Size(16, 32);
+            this.pb_Player.TabIndex = 0;
+            this.pb_Player.TabStop = false;
+            // 
+            // timer_Gravity
+            // 
+            this.timer_Gravity.Enabled = true;
+            this.timer_Gravity.Interval = 1;
+            this.timer_Gravity.Tick += new System.EventHandler(this.timer_Gravity_Tick);
+            // 
+            // timer_Jump
+            // 
+            this.timer_Jump.Enabled = true;
+            this.timer_Jump.Interval = 1;
+            this.timer_Jump.Tick += new System.EventHandler(this.timer_Jump_Tick);
+            // 
+            // timer_Anim
+            // 
+            this.timer_Anim.Enabled = true;
+            this.timer_Anim.Interval = 1;
+            this.timer_Anim.Tick += new System.EventHandler(this.timer_Anim_Tick);
+            // 
+            // timer_Sec
+            // 
+            this.timer_Sec.Enabled = true;
+            this.timer_Sec.Interval = 1000;
+            this.timer_Sec.Tick += new System.EventHandler(this.timer_Sec_Tick);
+            // 
+            // WorldFloor
+            // 
+            this.WorldFloor.BackgroundImage = global::BombsAway.Properties.Resources.floor;
+            this.WorldFloor.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.WorldFloor.Location = new System.Drawing.Point(0, 240);
+            this.WorldFloor.Name = "WorldFloor";
+            this.WorldFloor.Size = new System.Drawing.Size(656, 32);
+            this.WorldFloor.TabIndex = 1;
+           
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(656, 272);
+            this.Controls.Add(this.WorldFloor);
+            this.Controls.Add(this.WorldFrame);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = " ";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            this.WorldFrame.ResumeLayout(false);
+            this.WorldFrame.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pb_Block1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb_Block2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb_Player)).EndInit();
+            this.WorldFloor.ResumeLayout(false);
+            this.WorldFloor.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+        private System.Windows.Forms.Panel WorldFrame;
+        private System.Windows.Forms.Panel WorldFloor;
+
+        private System.Windows.Forms.Timer timer_Gravity;
+        private System.Windows.Forms.Timer timer_Jump;
+        private System.Windows.Forms.Timer timer_Anim;
+
+        private System.Windows.Forms.Label label1;
+
+        //private System.Windows.Forms.PictureBox WorldFrame;
+        //private System.Windows.Forms.PictureBox WorldFloor;
+        private System.Windows.Forms.PictureBox pb_Block1;
+        private System.Windows.Forms.PictureBox pb_Block2;
+        private System.Windows.Forms.PictureBox pb_Player;
+        private System.Windows.Forms.Label label_Score;
+    }
+}
+
---
+++ EngineeringProject/EngineeringProject/Form1.cs
@@ -0,0 +1,264 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EngineeringProject
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        PictureBox[] WorldObjects = new PictureBox[10];
+        Boolean Player_Jump = false;    //Is the player jumping
+        Boolean Player_Left = false;    //.. moving to the left
+        Boolean Player_Right = false;   //.. moving to the right
+        Boolean LastDirRight = true;    // Whats the last dir facing
+        Boolean GameOn = false;
+
+        int Gravity = 20;
+        int Anim = 0;
+        int Force = 0;
+        int Speed_Movement = 3;
+        int Speed_Jump = 3;
+        int Speed_Fall = 3;
+        int Score = 0;
+
+        public Boolean InAirNoCollision(PictureBox tar)
+        {   //Checks if the target Picturebox is Outside of the frame
+            if (!OutsideWorldFrame(tar))
+            {
+                foreach (PictureBox Obj in WorldObjects)
+                {   //Or if it's not colliding with anything
+                    if (!tar.Bounds.IntersectsWith(Obj.Bounds))
+                    {
+                        if (tar.Location.Y < WorldFrame.Width)
+                        {   //And it's not under ground for some reason
+                            return true;
+                        }
+                    }
+                }
+            }
+            return false;
+        }
+
+        public Boolean OutsideWorldFrame(PictureBox tar)
+        {
+            if (tar.Location.X < 0) //Is it outside of the left side?
+                return true;
+            if (tar.Location.X > WorldFrame.Width)  //... right side?
+                return true;
+            if (tar.Location.Y + tar.Height > WorldFrame.Height - 3)
+                return true;                        //Or above the WorldFrame?
+            foreach (PictureBox Obj in WorldObjects)
+            {
+                if (Obj != null)
+                {   //Or, intersecting with any world object
+                    if (tar.Bounds.IntersectsWith(Obj.Bounds))
+                        return true;
+                }
+            }
+            return false;
+        }
+
+        public Boolean Collision_Top(PictureBox tar)
+        {
+            foreach (PictureBox ob in WorldObjects)
+            {
+                if (ob != null)
+                {
+                    PictureBox temp1 = new PictureBox();    //Creates a single pixel above the target picturebox, asks if anything is colliding with it
+                    temp1.Bounds = ob.Bounds;
+                    //PaintBox(temp1.Location.X, temp1.Location.Y - 1, temp1.Width, 1, Color.Blue); //Super laggy doing this, troubleshooting only
+                    temp1.SetBounds(temp1.Location.X - 3, temp1.Location.Y - 1, temp1.Width + 6, 1);
+                    if (tar.Bounds.IntersectsWith(temp1.Bounds))
+                        return true;
+                }
+            }
+            return false;
+        }
+
+        public Boolean Collision_Bottom(PictureBox tar)
+        {
+            foreach (PictureBox ob in WorldObjects)
+            {
+                if (ob != null)
+                {
+                    PictureBox temp1 = new PictureBox();
+                    temp1.Bounds = ob.Bounds;
+                    //PaintBox(temp1.Location.X, temp1.Location.Y+temp1.Height, temp1.Width, 1, Color.Red); //Super laggy doing this, troubleshooting only
+                    temp1.SetBounds(temp1.Location.X, temp1.Location.Y + temp1.Height, temp1.Width, 1);
+                    if (tar.Bounds.IntersectsWith(temp1.Bounds))
+                        return true;
+                }
+            }
+            return false;
+        }
+
+        public Boolean Collision_Left(PictureBox tar)
+        {
+            foreach (PictureBox ob in WorldObjects)
+            {
+                if (ob != null)
+                {
+                    PictureBox temp1 = new PictureBox();
+                    temp1.Bounds = ob.Bounds;
+                    //PaintBox(temp1.Location.X - 1, temp1.Location.Y + 1, 1, temp1.Height - 1, Color.Green); //Super laggy doing this, troubleshooting only
+                    temp1.SetBounds(temp1.Location.X - 1, temp1.Location.Y + 1, 1, temp1.Height - 1);
+                    if (tar.Bounds.IntersectsWith(temp1.Bounds))
+                        return true;
+                }
+            }
+            return false;
+        }
+
+        public Boolean Collision_Right(PictureBox tar)
+        {
+            foreach (PictureBox ob in WorldObjects)
+            {
+                if (ob != null)
+                {
+                    PictureBox temp2 = new PictureBox();
+                    temp2.Bounds = ob.Bounds;
+                    //PaintBox(temp2.Location.X + temp2.Width, temp2.Location.Y + 1, 1, temp2.Height - 1, Color.Yellow); //Super laggy doing this, troubleshooting only
+                    temp2.SetBounds(temp2.Location.X + temp2.Width, temp2.Location.Y + 1, 1, temp2.Height - 1);
+                    if (tar.Bounds.IntersectsWith(temp2.Bounds))
+                        return true;
+                }
+            }
+            return false;
+        }
+        public void Reset()
+        {   //Resets everything
+
+            pb_Player.Visible = true;   //Sets the player visible and moves him to start location
+            pb_Player.Location = new System.Drawing.Point(167, WorldFrame.Size.Height - 10 - pb_Player.Height);
+            pb_Player.Image = Character.stand_r;
+            Score = 0;
+            GameOn = true;
+        }
+        public void PaintBox(int X, int Y, int W, int H, Color C)
+        {   //Just for troubleshooting purposes
+            PictureBox Temp = new PictureBox();
+            Temp.BackColor = C;
+            Temp.Size = new Size(W, H);
+            Temp.Location = new Point(X, Y);
+            WorldFrame.Controls.Add(Temp);
+        }
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.Z:
+                    Score += 120;
+                    break;
+                case Keys.X:
+                    pb_Player.Top += 3;
+                    break;
+                case Keys.Left:                 // On Left Keypress down
+                    if (GameOn)
+                    {
+                        LastDirRight = false;   //For the animation, stand right or left
+                        Player_Left = true;     //Walk left
+                    }
+                    break;
+                case Keys.Right:                // On Right Keypress down
+                    if (GameOn)
+                    {
+                        LastDirRight = true;
+                        Player_Right = true;
+                    }
+                    break;
+                case Keys.Space:    // On Space Keypress down
+                    
+                        if (!Player_Jump && !InAirNoCollision(pb_Player))
+                        {   //Anti multijump - If the player doesnt jump, is in the air and not colliding with anything
+                            if (LastDirRight)       //Checks direction, changes jump image
+                            {
+                                pb_Player.Image = Character.jump_r;
+                            }
+                            else
+                            {
+                                pb_Player.Image = Character.jump_l;
+                            }
+                            pb_Player.Top -= Speed_Jump;     //Player moves up a bit
+                            Force = Gravity;        //Force to be moved up changes
+                            Player_Jump = true;     //Sets a variable that player is jumping
+                        }
+                    
+                    break;
+            }
+            private void Form1_KeyUp(object sender, KeyEventArgs e)
+            {
+                if (GameOn)
+                {
+                    switch (e.KeyCode)
+                    {
+                        case Keys.Left:                             //On Left Key press UP
+                            pb_Player.Image = Character.stand_l;    //Players image changes to stand
+                            LastDirRight = false;                   //Last move was to the left
+                            Player_Left = false;                    //Doesnt move left anymore
+                            break;
+                        case Keys.Right:
+                            pb_Player.Image = Character.stand_r;
+                            LastDirRight = true;
+                            Player_Right = false;
+                            break;
+                    }
+                }
+            }
+        }
+
+        private void timer_Jump_Tick(object sender, EventArgs e)
+        {
+            if (GameOn)
+            {
+                if (Player_Right && pb_Player.Right <= WorldFrame.Width - 3 && !Collision_Left(pb_Player))
+                { //Stops the player from moving out of screen
+                    pb_Player.Left += Speed_Movement; //Moves right
+                }
+                if (Player_Left && pb_Player.Location.X >= 3 && !Collision_Right(pb_Player))
+                { //Stops the player from moving out of screen
+                    pb_Player.Left -= Speed_Movement; //Moves left
+                }
+            }
+            else
+            {   //If game is not on, stops the player
+                Player_Right = false;
+                Player_Left = false;
+            }
+
+            if (Force > 0)
+            {   //If any force still exists
+                if (Collision_Bottom(pb_Player))
+                {   //Unless players head is banging in a wall
+                    Force = 0;
+                }
+                else
+                {   //Move player up, lower force each "move"
+                    Force--;
+                    pb_Player.Top -= Speed_Jump;
+                }
+            }
+            else
+            {   //If no force, player is not jumping.
+                Player_Jump = false;
+            }
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Reset();
+            WorldObjects[1] = pb_Block1;
+            WorldObjects[2] = pb_Block2;
+        }
+    }
+}
