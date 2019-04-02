8fd9f01de3496a5142613ca968428d5449d2b4b5
ElGrell <48662508+ElGrell@users.noreply.github.com>
2019-04-02 06:03:51 +0300

    Add files via upload
    I couldn't upload the whole project because it freezes on the 25th file every time, no matter what it is.
    Here's a link to it on my Google drive 
    https://drive.google.com/open?id=1d4h9uRayJaqhNzNkY5vG4Wj1jiVqHS0e

---
+++ Platformer002/Form1.Designer.cs
@@ -0,0 +1,133 @@
+﻿namespace Platformer002
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pbGround = new System.Windows.Forms.PictureBox();
+            this.pbPlayer = new System.Windows.Forms.PictureBox();
+            this.tmrGravity = new System.Windows.Forms.Timer(this.components);
+            this.tmrUp = new System.Windows.Forms.Timer(this.components);
+            this.tmrRight = new System.Windows.Forms.Timer(this.components);
+            this.tmrLeft = new System.Windows.Forms.Timer(this.components);
+            this.lblScore = new System.Windows.Forms.Label();
+            this.tmrGameLoop = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pbGround)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbPlayer)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pbGround
+            // 
+            this.pbGround.BackColor = System.Drawing.Color.LimeGreen;
+            this.pbGround.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.pbGround.Location = new System.Drawing.Point(0, 367);
+            this.pbGround.Name = "pbGround";
+            this.pbGround.Size = new System.Drawing.Size(800, 83);
+            this.pbGround.TabIndex = 0;
+            this.pbGround.TabStop = false;
+            // 
+            // pbPlayer
+            // 
+            this.pbPlayer.BackColor = System.Drawing.Color.Indigo;
+            this.pbPlayer.Location = new System.Drawing.Point(31, 264);
+            this.pbPlayer.Name = "pbPlayer";
+            this.pbPlayer.Size = new System.Drawing.Size(51, 74);
+            this.pbPlayer.TabIndex = 1;
+            this.pbPlayer.TabStop = false;
+            // 
+            // tmrGravity
+            // 
+            this.tmrGravity.Enabled = true;
+            this.tmrGravity.Interval = 10;
+            this.tmrGravity.Tick += new System.EventHandler(this.tmrGravity_Tick);
+            // 
+            // tmrUp
+            // 
+            this.tmrUp.Interval = 10;
+            this.tmrUp.Tick += new System.EventHandler(this.tmrUp_Tick);
+            // 
+            // tmrRight
+            // 
+            this.tmrRight.Interval = 10;
+            this.tmrRight.Tick += new System.EventHandler(this.tmrRight_Tick);
+            // 
+            // tmrLeft
+            // 
+            this.tmrLeft.Interval = 10;
+            this.tmrLeft.Tick += new System.EventHandler(this.tmrLeft_Tick);
+            // 
+            // lblScore
+            // 
+            this.lblScore.AutoSize = true;
+            this.lblScore.Font = new System.Drawing.Font("Miriam Libre", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblScore.Location = new System.Drawing.Point(12, 9);
+            this.lblScore.Name = "lblScore";
+            this.lblScore.Size = new System.Drawing.Size(91, 28);
+            this.lblScore.TabIndex = 2;
+            this.lblScore.Text = "Score: 0";
+            // 
+            // tmrGameLoop
+            // 
+            this.tmrGameLoop.Enabled = true;
+            this.tmrGameLoop.Interval = 10;
+            this.tmrGameLoop.Tick += new System.EventHandler(this.tmrGameLoop_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.SkyBlue;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblScore);
+            this.Controls.Add(this.pbPlayer);
+            this.Controls.Add(this.pbGround);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            ((System.ComponentModel.ISupportInitialize)(this.pbGround)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbPlayer)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pbGround;
+        private System.Windows.Forms.PictureBox pbPlayer;
+        private System.Windows.Forms.Timer tmrGravity;
+        private System.Windows.Forms.Timer tmrUp;
+        private System.Windows.Forms.Timer tmrRight;
+        private System.Windows.Forms.Timer tmrLeft;
+        private System.Windows.Forms.Label lblScore;
+        private System.Windows.Forms.Timer tmrGameLoop;
+    }
+}
+
---
+++ Platformer002/Form1.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Platformer002
+{
+    public partial class Form1 : Form
+    {
+        bool isJumping = false;
+        List<Coin> cList = new List<Coin>();
+        int score = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void tmrGravity_Tick(object sender, EventArgs e)
+        {
+            if (!pbPlayer.Bounds.IntersectsWith(pbGround.Bounds) && isJumping == false)
+            {
+                pbPlayer.Top += 10;
+            }
+        }
+
+        private void tmrUp_Tick(object sender, EventArgs e)
+        {
+            pbPlayer.Top -= 10;
+            isJumping = true;
+        }
+
+        private void tmrRight_Tick(object sender, EventArgs e)
+        {
+            pbPlayer.Left += 10;
+        }
+
+        private void tmrLeft_Tick(object sender, EventArgs e)
+        {
+            pbPlayer.Left -= 10;
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Up)
+            {
+                tmrUp.Start();
+            }
+            else if (e.KeyCode == Keys.Right)
+            {
+                tmrRight.Start();
+            }
+            else if (e.KeyCode == Keys.Left)
+            {
+                tmrLeft.Start();
+            }
+        }
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Up)
+            {
+                tmrUp.Stop();
+                isJumping = false;
+            }
+            else if (e.KeyCode == Keys.Right)
+            {
+                tmrRight.Stop();
+            }
+            else if (e.KeyCode == Keys.Left)
+            {
+                tmrLeft.Stop();
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Coin c1 = new Coin();
+            Coin c2 = new Coin();
+            Coin c3 = new Coin();
+            Coin c4 = new Coin();
+            Coin c5 = new Coin();
+            Coin c6 = new Coin();
+            Coin c7 = new Coin();
+
+            c1.drawTo(this);
+            cList.Add(c1);
+            c1.setPos(100, 200);
+
+            c2.drawTo(this);
+            cList.Add(c2);
+            c2.setPos(200, 200);
+
+            c3.drawTo(this);
+            cList.Add(c3);
+            c3.setPos(300, 200);
+
+            c4.drawTo(this);
+            cList.Add(c4);
+            c4.setPos(400, 200);
+
+            c5.drawTo(this);
+            cList.Add(c5);
+            c5.setPos(500, 200);
+
+            c6.drawTo(this);
+            cList.Add(c6);
+            c6.setPos(600, 200);
+
+            c7.drawTo(this);
+            cList.Add(c7);
+            c7.setPos(700, 200);
+
+        }
+
+        private void tmrGameLoop_Tick(object sender, EventArgs e)
+        {
+            foreach (Coin c in cList)
+            {
+                if (pbPlayer.Bounds.IntersectsWith(c.getBounds()))
+                {
+                    c.setPos(1001, 1001);
+                    score+=10;
+                    lblScore.Text = "Score: " + score;
+                }
+            }
+        }
+    }
+}
